abb4cdb8fcda558e41100ac05ace311e4deb7aad
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-04-04 14:47:05 +0900

    整形

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   root "home#top"
-
-
-
-  # get 'users/index'
-  # post "users/:id/destroy" => "users#destroy"
-  # post "users/create" => "users#create"
   resources:users
 end
