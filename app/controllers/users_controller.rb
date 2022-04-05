ba0dd8b7c73d00babb1eaa2dd99e129d54001301
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-04-05 20:44:59 +0900

    before actionを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -18,15 +21,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(params.require(:user).permit(:name, :email, :password))
       flash[:notice] = "更新しました"
       redirect_to user_path(@user.id)
@@ -36,9 +39,15 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
+    
     @user.destroy
     flash[:notice] = "ユーザーを削除しました"
     redirect_to users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
