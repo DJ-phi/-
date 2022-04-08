a25d35f04d64ca45dd6afce5c50d4d800f70aa7c
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-04-08 19:29:28 +0900

    ログイン機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_current_user
 
   def index
     @users = User.all
@@ -13,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to user_path(@user.id)
     else
@@ -21,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
-    
   end
 
   def update
@@ -38,10 +38,35 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    
     @user.destroy
     flash[:notice] = "ユーザーを削除しました"
     redirect_to users_path
+  end
+
+  def login_form
+
+  end
+
+  def login
+    @user = User.find_by(
+      email: params[:email],
+      password: params[:password]
+    )
+    if @user 
+      session[:user_id] = @user.id  #ここに追加
+      flash[:notice] = "ログインしました"
+      redirect_to user_path(@user.id)
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      render("users/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
 
   private
@@ -52,5 +77,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "home#top"
+
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
   resources:users
 end
