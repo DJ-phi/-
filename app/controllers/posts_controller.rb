b371a5c14b131d990d44e2037a881a84a6c3dc71
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-04-29 00:40:55 +0900

    post実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   # before_action :authenticate_user 後ほど解放
 
   def index
@@ -7,15 +8,21 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "投稿できました"
+      redirect_to post_path(@post.id)
+    else
+      render :new
+    end
   end
 
   def show
-
+    @posts = Post.all
   end
 
   def edit
@@ -23,10 +30,27 @@ class PostsController < ApplicationController
   end
 
   def update
-
+    if @post.update(post_params)
+      flash[:notice] = "更新しました"
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post.destroy
+    flash[:notice] = "削除しました"
+    redirect_to postss_path
+  end
 
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :food, :traffic)
   end
 end
