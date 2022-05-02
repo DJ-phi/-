678db3571e0517e01c520b8017a87f26858b31ca
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-05-03 02:10:44 +0900

    半角数字のバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   # belongs_to :user #リレーション, userは1の関係性
 
+  validates :traffic, :food, format: { with: /\A[0-9]+\z/ }
+
   def total_price
     traffic + food
   end
