c5c5bd0e9770446fa5c3ef09d0f2694e1513c4e1
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-05-31 18:59:32 +0900

    記述変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
   def self.search(search)
     if search
       #数字の検索は文字列にしないとエラーになるcast(price as text)でテキストに変換されている
-      Post.joins(:category).where( 'cast(use_day as text) LIKE ? OR cast(price as text) LIKE ? OR memo LIKE ? OR name LIKE ?',  "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%" )
+      Post.joins(:category).where( 'cast(created_at as text) LIKE ? OR cast(use_day as text) LIKE ? OR cast(price as text) LIKE ? OR memo LIKE ? OR name LIKE ?',  "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%" )
     else
       Post.all
     end
@@ -21,7 +21,3 @@ class Post < ApplicationRecord
   # scope :
   # scope :young, -> { where("age < 20") }
 end
-
-# Room.where(['room_name LIKE ? OR room_area LIKE ? OR room_memo LIKE ? ', "%#{search}%", "%#{search}%", "%#{search}%"])参考文
-# @books = Book.page(params[:page]).per(100).where('genre1 LIKE ?', "%#{params[:genre1]}%").where('genre2 LIKE ?', "%#{params[:genre2]}%")...
-# @books = Book.page(params[:page]).per(100).where('cast(genre1 as text) LIKE ?', "%#{params[:genre1]}%").where('genre2 LIKE ?', "%#{params[:genre2]}%")...
