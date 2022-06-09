9780d3fcc9fa2de7ee9b5e3a338bc70650caf52f
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-09 17:39:10 +0900

    sessionのメモ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
+    # ページを移動してもユーザー情報を保持し続けるために、sessionという特殊な変数を用います。
+    # sessionに代入された値は、ブラウザ(InternetExplorer, GoogleChrome等)に保存されます。
+    # sessionに値を代入すると、ブラウザはそれ以降のアクセスでsessionの値をRailsに送信します。
     @current_user = User.find_by(id: session[:user_id])
   end
 
