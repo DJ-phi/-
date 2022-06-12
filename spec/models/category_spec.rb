65e256894a53c37b2640c620cb77be7c737c595e
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-12 20:14:58 +0900

    バリデーションテスト　テスト追加

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = create(:user)
+    @category = create(:category)
+    @post = create(:post)
+  end
+
+  describe "バリデーション" do
+    it "[name]空だったらNG" do
+      @category.name = ""
+      expect(@category.valid?).to eq(false)
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   before do
-
+    @user = create(:user)
+    @category = create(:category)
+    @post = create(:post)
   end
 
   describe "バリデーション" do
-    it "数字以外はバリデーションがかかること" do
-
+    it "[price]数字以外はバリデーションがかかること" do
+      @post.price = ""
+      expect(@post.valid?).to eq(false)
     end
   end
 end
