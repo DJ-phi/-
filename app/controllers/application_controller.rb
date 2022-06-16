004773607ff96274903aa767fe018bdb74c78c47
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-16 17:13:26 +0900

    404ページ仕様変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,6 @@ class ApplicationController < ActionController::Base
 
   before_action :set_current_user
 
-  # 例外ハンドル
-  unless Rails.env.development?
-    rescue_from Exception,                      with: :_render_500
-    rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
-    rescue_from ActionController::RoutingError, with: :_render_404
-  end
-
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
-
   def set_current_user
     # ページを移動してもユーザー情報を保持し続けるために、sessionという特殊な変数を用います。
     # sessionに代入された値は、ブラウザ(InternetExplorer, GoogleChrome等)に保存されます。
@@ -31,28 +20,6 @@ class ApplicationController < ActionController::Base
     if @current_user
       # showに飛ばしたい時は@current_userが主流
       redirect_to user_path(@current_user)
-    end
-  end
-
-  private
-
-  def _render_404(e = nil)
-    logger.info "Rendering 404 with excaption: #{e.message}" if e
-
-    if request.format.to_sym == :json
-      render json: { error: "404 Not Found" }, status: :not_found
-    else
-      render "errors/404.html", status: :not_found, layout: "error"
-    end
-  end
-
-  def _render_500(e = nil)
-    logger.error "Rendering 500 with excaption: #{e.message}" if e
-
-    if request.format.to_sym == :json
-      render json: { error: "500 Internal Server Error" }, status: :internal_server_error
-    else
-      render "errors/500.html", status: :internal_server_error, layout: "error"
     end
   end
 end
