eb8866deb23cd46e0d670add4e09b2a67047acad
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-17 16:19:24 +0900

    テスト　ok

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
+  let(:user) { create(:user) }
+
   describe "GET /top" do
     it "ステータスコードが200になること" do
       get root_path
       expect(response).to have_http_status(:success)
     end
 
-    # TODO: 終わってない
     it "ログイン済の場合、リダイレクトされる" do
+      login
       get root_path
-      expect(response).to redirect_to(user_path(current_user))
+      expect(response).to redirect_to(user_path(user))
     end
   end
 end
