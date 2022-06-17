7e9ad53b2bdbd59c6630468245064248a4bfbca7
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-18 00:15:53 +0900

    不要なカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_124047) do
+ActiveRecord::Schema.define(version: 2022_06_17_151319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2022_06_04_124047) do
     t.integer "category_id"
     t.integer "price", default: 0
     t.date "use_day"
-    t.string "category_name"
   end
 
   create_table "users", force: :cascade do |t|
