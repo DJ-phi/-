692b5b054f86cc37826b3a377abb011ccd87698f
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-19 22:48:53 +0900

    factory仕様変更

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -32,14 +32,33 @@ FactoryBot.define do
       user_id { 1 }
       category_id { 1 }
     end
+
+    trait :find_post1 do
+      memo { "すき家" }
+      price { 10111111 }
+      use_day { "2022-06-10" }
+    end
+
+    trait :find_post2 do
+      memo { "アイス" }
+      price { 200 }
+      use_day { "2022-06-20" }
+    end
+
+    trait :find_post3 do
+      memo { "お茶" }
+      price { 5000 }
+      use_day { "2022-06-30" }
+    end
   end
 
-  factory :find_post, class: Post do
-    memo { "御飯" }
-    price { 100 }
-    user_id { 1 }
-    category_id { 1 }
-    use_day { "2022-06-01" }
+  # TODO:まだ使うか分からないから残し
+  # factory :find_post, class: Post do
+  #   memo { "御飯" }
+  #   price { 100 }
+  #   user_id { 1 }
+  #   category_id { 1 }
+  #   use_day { "2022-06-01" }
 
   #   trait :find_post1 do
   #     memo { "すき家" }
@@ -58,5 +77,5 @@ FactoryBot.define do
   #     price { 5000 }
   #     use_day { "2022-06-30" }
   #   end
-  end
+  # end
 end
