974850c19adb8b592336c9821512e4a05c372cd8
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-22 01:19:46 +0900

    固定値に変更

--- db/fixtures/01_user.rb
+++ db/fixtures/01_user.rb
@@ -1,7 +1,15 @@
-10.times do |n|
-  User.seed do |s|
-    s.name = "テスト名前#{n}"
-    s.email = "aaa#{n}@gmail.com"
-    s.password = "123456"
+User.seed do |s|
+  s.id = 1
+  s.name = "テスト"
+  s.password = 1234
   end
-end
+
+# 複数で書きたい時の記述
+# idを入れないで生成すると自動で123...と作ってくれることを確認した
+# 10.times do |n|
+#   User.seed do |s|
+#     s.name = "テスト名前#{n}"
+#     s.email = "aaa#{n}@gmail.com"
+#     s.password = "123456"
+#   end
+# end
