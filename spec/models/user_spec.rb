0f9b00b4edf36da7c095aa171f342cc156d1b281
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-06-27 14:42:12 +0900

    インスタンス変数からletに変更　テストケース追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,63 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  before do 
-    @user = create(:user)
-  end
+  let!(:user) { create(:user) }
 
   describe "バリデーション" do
     it "名前とemail, passwordの値が設定されていれば、OK" do
-      expect(@user.valid?).to eq(true)
+      expect(user.valid?).to eq(true)
     end
 
     it "nameが被っていたらNG" do
-      @user.save
-      @user2 = build(:user)
-      @user2.name = @user.name
-      expect(@user2.valid?).to eq(false)
+      user.save
+      user2 = build(:user)
+      user2.name = user.name
+      expect(user2.valid?).to eq(false)
     end
 
     it "nameの10文字制限" do
-      @user.name = "1234567891234"
-      expect(@user.valid?).to eq(false)
+      user.name = "1234567891234"
+      expect(user.valid?).to eq(false)
     end
 
     it "nameが空だとNG" do
-      @user.name = ""
-      expect(@user.valid?).to eq(false)
+      user.name = ""
+      expect(user.valid?).to eq(false)
     end
 
     it "emailが空だとNG" do
-      @user.email = ""
-      expect(@user.valid?).to eq(false)
+      user.email = ""
+      expect(user.valid?).to eq(false)
     end
 
     it "emailが重複しているとNG" do
       # mここでセーブしないと重複したデータが作れない
-      @user.save
-      @user2 = build(:user)
-      @user2.email = @user.email
-      expect(@user2.valid?).to eq(false)
+      user.save
+      user2 = build(:user)
+      user2.email = user.email
+      expect(user2.valid?).to eq(false)
     end
 
     it "passwordが空だとNG" do
-      @user.password = ""
-      expect(@user.valid?).to eq(false)
+      user.password = ""
+      expect(user.valid?).to eq(false)
     end
 
     it"passwordが4文字以下だったらNG" do
-      @user.password = "123"
-      expect(@user.valid?).to eq(false)
+      user.password = "123"
+      expect(user.valid?).to eq(false)
     end
 
     it"passwordが10文字以上だったらNG" do
-      @user.password = "1234567891011"
-      expect(@user.valid?).to eq(false)
+      user.password = "1234567891011"
+      expect(user.valid?).to eq(false)
     end
+  end
 
+  describe "モデルのオプションdependent: :destroyのテスト" do
     it "userを消したらpostも消えること" do
-
+      category = create(:category)
+      post = create(:post)
+      user.destroy
+      expect(Post.first).to eq(nil)
     end
   end
 end
