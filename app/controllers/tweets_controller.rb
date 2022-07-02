5e8e89e9686713d546f2ff79ca039ab75d201801
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-07-02 21:09:49 +0900

    一式雛形作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,25 @@
 class TweetsController < ApplicationController
   def index
+    
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
