b182dd677a9c81440abd3e11db933fc7b3af89cc
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-07-02 22:50:33 +0900

    new 作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,22 @@
 class TweetsController < ApplicationController
+
   def index
+    console
     @tweets = Tweet.all.order(created_at: :desc)
   end
 
   def new
-
+    @tweet = Tweet.new
   end
 
   def create
-
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "つぶやきました"
+      redirect_to tweets_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +29,11 @@ class TweetsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :user_id).merge(user_id: @current_user.id)
   end
 end
