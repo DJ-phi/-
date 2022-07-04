2b07a9984f918a806823b83b03ec27ef8b606e5f
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-07-04 21:37:01 +0900

    コントローラー作成 テスト先書き

--- spec/models/tweet_like_spec.rb
+++ spec/models/tweet_like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe TweetLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # before do
+  #   create(:user)
+  #   create(:category)
+  #   create(:post)
+  # end
+
+  # describe '正常値と異常値の確認' do
+  #   it "user_idとpost_idがあれば保存できる" do
+  #     expect(create(:like)).to be_valid
+  #   end
+  # end
 end
---
+++ spec/requests/tweet_likes_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "TweetLikes", type: :request do
+  # describe "#tweet_likes" do
+  #   it "データが作成されること" do
+  #     expect do
+  #       post "/like/#{new_post.id}.js"
+  #     end.to change(Like, :count).by(1)
+  #   end
+  # end
+
+  # describe "#destroy" do
+  #   let!(:like) { create(:like) }
+  #   it "データが削除されること" do
+  #     expect do
+  #       delete "/like/#{new_post.id}.js"
+  #     end.to change(Like, :count).by(-1)
+  #   end
+  # end
+end
