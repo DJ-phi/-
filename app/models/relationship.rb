299c42453038899a9cb890336fd60c0b0ceea00b
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-07-12 01:54:13 +0900

    解説入れ

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,8 @@
 class Relationship < ApplicationRecord
+  # フォローする人(follower)もされる人(followed)もUserモデルでした。
+  # 本来、followerやfollowedといったモデルは存在しません。
+  # class_nameをつけることで、関連先のモデルを参照する際の名前を変更できるということです。
+  # 「Userをfollowとfollowedに分ける」ことをclass_nameがやってくれるんですね。
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 end
