7fd9320e189c1c854ddd41d1d12a2fd65cf967bd
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-12-23 22:35:55 +0900

    paramsのpassword名変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,7 +95,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password_digest)
   end
 
   def ensure_correct_user
